c74ca1749ed361c2d38f592d9878b0e95fa9a8c4
Troopermaniac <37369028+Troopermaniac@users.noreply.github.com>
2025-04-23 17:55:59 -0400

    Update ForceRailGeneration.cs

--- ForceRailGeneration/ForceRailGeneration.cs
+++ ForceRailGeneration/ForceRailGeneration.cs
@@ -8,35 +8,9 @@
 
 using HarmonyLib;
 using System;
-using System.Reflection;
-using UnityEngine;
 
 public class ForceRailGeneration
 {
-    private const string HarmonyId = "com.troop.forcerailgeneration";
-    private static Harmony _harmony;
-
-    [RuntimeInitializeOnLoadMethod]
-    private static void Initialize()
-    {
-        try
-        {
-            if (!World.Config.AboveGroundRails)
-            {
-                UnityEngine.Debug.LogWarning("[ForceRailGeneration] AboveGroundRails is disabled. Enabling it.");
-                World.Config.AboveGroundRails = true;
-            }
-
-            _harmony = new Harmony(HarmonyId);
-            _harmony.PatchAll(Assembly.GetExecutingAssembly());
-            UnityEngine.Debug.Log("[ForceRailGeneration] Harmony patch applied for rail generation.");
-        }
-        catch (Exception ex)
-        {
-            UnityEngine.Debug.LogError($"[ForceRailGeneration] Error initializing: {ex.Message}");
-        }
-    }
-
     [HarmonyPatch(typeof(GenerateRailRing), "Process")]
     public class RailPatch
     {
